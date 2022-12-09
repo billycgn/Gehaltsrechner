9aefacb02bfd1018e3ddfe16b7855b908134d893
Billy Koch <billy.koch@gmx.de>
2022-12-09 11:21:03 +0100

    Create Gehaltsrechner.py

---
+++ Gehaltsrechner.py
@@ -0,0 +1,8 @@
+stunde=int(input("Stundenlohn eingebn:"))
+tageslohn=(stunde*8)
+monat=(tageslohn*30)
+jahr=(monat*12)
+
+print("Sie verdienen " , tageslohn , " € am Tag")
+print("Sie verdienen " , monat , " € im Monat")
+print("Das ergibt ein Jahresgehalt von" , jahr , " €.")
